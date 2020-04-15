8054c35fe3c637c4ca5b4da8cb6bef4f2de86213
khoi148 <khoiminhphan@yahoo.com>
2020-04-15 11:12:46 +0700

    updated ENV and port config, get ready for herkou

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-app.listen(3001, () => {
+app.listen(process.env.PORT, () => {
   console.log("server is running on port 3001");
 });
 module.exports = app;
